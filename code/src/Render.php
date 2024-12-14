5639041fd9e79ff23e319a95f1ea72b06d136a6f
mamedovsh <shagin.mamedoff@yandex.ru>
2024-12-14 11:55:17 +0700

    Задание семинар №6

--- code/src/Render.php
+++ code/src/Render.php
@@ -4,6 +4,7 @@ namespace Geekbrains\Application1;
 
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
+use Exception;
 
 class Render {
 
@@ -11,12 +12,10 @@ class Render {
     private FilesystemLoader $loader;
     private Environment $environment;
 
-
     public function __construct(){
-
         $this->loader = new FilesystemLoader(dirname(__DIR__) . $this->viewFolder);
         $this->environment = new Environment($this->loader, [
-           // 'cache' => $_SERVER['DOCUMENT_ROOT'].'/cache/',
+            // 'cache' => $_SERVER['DOCUMENT_ROOT'].'/cache/',
         ]);
     }
 
@@ -27,5 +26,35 @@ class Render {
         $templateVariables['title'] = 'имя страницы';
 
         return $template->render($templateVariables);
+    }
+    
+    public static function renderExceptionPage(Exception $e) {
+        $templateVariables = [
+            'error_message' => $e->getMessage(),
+            'error_code' => $e->getCode(),
+        ];
+
+        $loader = new FilesystemLoader(dirname(__DIR__) . '/src/Views/');
+        $environment = new Environment($loader);
+        $template = $environment->load('/layouts/error.twig');
+
+        return $template->render($templateVariables);
+    }
+}
+
+class UserController {
+    
+    public function updateUser(array $data) {
+        if (!isset($data['id']) || !isset($data['name'])) {
+            throw new Exception('ID и имя пользователя обязательны');
+        }
+
+        $userId = (int)$data['id'];
+        $newName = $data['name'];
+return "Пользователь с ID $userId обновлён с новым именем: $newName";
+    }
+
+    public function deleteUser(int $userId) {
+        return "Пользователь с ID $userId удалён";
     }
 }
